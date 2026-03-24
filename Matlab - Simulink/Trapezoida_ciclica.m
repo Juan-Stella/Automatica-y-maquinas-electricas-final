17808cd21bc24d93f9ffb8195c6aeb6935896287
Juan-Stella <juanstella96@gmail.com>
2026-03-24 20:06:10 -0300

    .

--- Matlab - Simulink/Trapezoida_ciclica.m
+++ Matlab - Simulink/Trapezoida_ciclica.m
@@ -1,3 +1,7 @@
-omega_ref = out.Omega_ciclica
-t = omega_ref.time
-u = omega_ref.data
+omega_ref = out.Omega_ciclica;
+t = omega_ref.time;
+u = omega_ref.data;
+
+tld_ref = out.Tld_ciclica;
+t1 = tld_ref.time;
+u1 = tld_ref.data;
